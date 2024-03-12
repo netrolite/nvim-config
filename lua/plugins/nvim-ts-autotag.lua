bb920fc41b666d253baced3cc35f7bb419cfc496
netrolite <netrolite@proton.me>
2024-03-12 15:47:25 +0300

    add nvim-ts-autotag & nvim-web-devicons

---
+++ lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,15 @@
+return {
+  "windwp/nvim-ts-autotag",
+  ft = {
+    "html",
+    "htm",
+    "svelte",
+    "jsx",
+    "tsx",
+    "vue"
+  },
+  config = {},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter"
+  }
+}
