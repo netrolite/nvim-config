711a1a5a44b241495be04bdf2c4983495a78a943
netrolite <netrolite@proton.me>
2024-03-12 16:59:05 +0300

    add vim-highlightedyank & vim-illuminate

---
+++ lua/plugins/vim-illuminate.lua
@@ -0,0 +1,12 @@
+return {
+  "RRethy/vim-illuminate",
+  config = function()
+    require("illuminate").configure({
+      filetypes_denylist = { -- don't enable in these file types
+        "md"
+      },
+      min_count_to_highlight = 2,
+    })
+  end,
+  lazy = false
+}
