5f5548dd44ca9090c9e3af86b24e2470ac437e47
netrolite <netrolite@proton.me>
2024-03-13 16:08:24 +0300

    add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,29 @@
+local config = function()
+	require("lualine").setup({
+		options = {
+			theme = "auto",
+			globalstatus = true,
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch" },
+			lualine_c = { "location", "searchcount",  "selectioncount" },
+			lualine_x = { "filetype" },
+			lualine_y = { "fileformat" },
+			lualine_z = { "progress" },
+		},
+		tabline = {
+			lualine_a = { "buffers" },
+      lualine_y = { "windows" },
+      lualine_z = { "tabs" }
+    },
+	})
+end
+
+return {
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	config = config,
+}
