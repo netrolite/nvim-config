c6958353ef51220e7b62dd312e77493b411fc405
netrolite <netrolite@proton.me>
2024-03-17 09:03:42 +0300

    fix window resize keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,11 +5,10 @@ mapKey("<C-j>", "<C-w>j")
 mapKey("<C-k>", "<C-w>k")
 mapKey("<C-h>", "<C-w>h")
 mapKey("<C-l>", "<C-w>l")
-mapKey("<C-Up>", ":resize -2<CR>")
-mapKey("<C-Down>", ":resize +2<CR>")
-mapKey("<C-Left>", ":resize vertical +2<CR>")
-mapKey("<C-Right>", ":resize vertical -2<CR>")
-
+mapKey("<C-[>", ":resize -2<CR>")
+mapKey("<C-]>", ":resize +2<CR>")
+mapKey("<C-->", ":vertical resize +2<CR>")
+mapKey("<C-=>", ":vertical resize -2<CR>")
 
 -- splits
 mapKey("<leader>sh", ":split<CR>")
