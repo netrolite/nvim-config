4c9a06ab0d14c3bdf074568f0757b2e644f2796f
netrolite <netrolite@proton.me>
2024-03-17 14:16:52 +0300

    improve mapKey util fn

--- lua/utils/mapKey.lua
+++ lua/utils/mapKey.lua
@@ -1,47 +1,20 @@
-local vimModes = {
-	n = "n",
-	i = "i",
-	v = "v",
-}
-
-local defaultOpts = {
+local default_opts = {
 	noremap = true,
 	silent = true,
 }
 
---- @param opts (table|nil)
---- @return table
-local getOpts = function(opts)
-	local allOpts = opts
-	if allOpts == nil then
-		allOpts = {}
-	end
-	for k, v in pairs(defaultOpts) do
-		allOpts[k] = allOpts[k] or v
-	end
-	return allOpts
-end
+function mapKey(keymaps, command, mode_and_opts)
+  mode_and_opts = mode_and_opts or {}
+	local mode = mode_and_opts.mode or "n"
 
---- @param vimMode (string|nil)
---- @return string
-local getMode = function(vimMode)
-	local modeString = vimModes[vimMode]
-	if modeString == nil then
-		return "n"
-	else
-		return modeString
-	end
-end
+	local opts
+  if mode_and_opts.opts then
+    opts = vim.tbl_deep_extend("force", default_opts, mode_and_opts.opts)
+  else
+    opts = default_opts
+  end
 
---- @param keymaps string
---- @param command string
---- @param vimMode (string|nil)
---- @param options (table|nil)
---- @return nil
-function mapKey(keymaps, command, vimMode, options)
-	local mode = getMode(vimMode)
-	local opts = getOpts(options)
-	vim.keymap.set(mode, keymaps, command, opts)
+	vim.keymap.set(mode, keymaps, command, merged_opts)
 end
 
 return mapKey
