c7e42b338018e119d361c70a1e382a00b8d22e8d
netrolite <netrolite@proton.me>
2024-03-17 14:16:59 +0300

    properly configure nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,14 +2,32 @@ return {
   "nvim-tree/nvim-tree.lua",
   lazy = false,
   opts = {
-    update_focused_file = {
-      enable = true,
-    },
+    hijack_cursor = true,
+    select_prompts = true,
+    update_focused_file = { enable = true },
     view = {
-      relativenumber = true
+      relativenumber = true,
+      cursorline = true,
+      width = 45,
+    },
+    renderer = {
+      root_folder_label = ":t",
+      highlight_bookmarks = "name",
+      indent_markers = {
+        enable = true,
+        inline_arrows = false
+      }
+    },
+    diagnostics = {
+      enable = true
+    },
+    filters = {
+      exclude = { -- always show
+        ".env*", "node_modules"
+      }
     }
   },
   keys = {
-    { "<leader>N", ":NvimTreeToggle<CR>"}
+    { "<leader>N", ":NvimTreeToggle<CR>" }
   }
 }
