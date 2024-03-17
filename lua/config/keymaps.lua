63b2b419bfb01b86d99c48e79528b6b51b0e8a12
netrolite <netrolite@proton.me>
2024-03-17 14:15:12 +0300

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,11 +5,10 @@ mapKey("<C-j>", "<C-w>j")
 mapKey("<C-k>", "<C-w>k")
 mapKey("<C-h>", "<C-w>h")
 mapKey("<C-l>", "<C-w>l")
-mapKey("<C-=>", ":resize +2<CR>")
-mapKey("<C-->", ":resize -2<CR>")
-mapKey("<C-]>", ":vertical resize -2<CR>")
-mapKey("<C-[>", ":vertical resize +2<CR>")
-
+mapKey("<leader>=", ":resize +2<CR>")
+mapKey("<leader>-", ":resize -2<CR>")
+mapKey("<leader>]", ":vertical resize -2<CR>")
+mapKey("<leader>[", ":vertical resize +2<CR>")
 
 -- splits
 mapKey("<leader>sh", ":split<CR>")
@@ -27,12 +26,16 @@ mapKey(">", ">gv", "v")
 mapKey("<", "<<")
 mapKey(">", ">>")
 
--- switching buffers
-mapKey("<C-i>", ":bp<CR>")
-mapKey("<C-o>", ":bn<CR>")
-
 -- format
 mapKey("<leader>fm", ":lua vim.lsp.buf.format()<CR>")
 
 -- no highlight
 mapKey("<leader>nh", ":noh<CR>")
+
+-- move text up and down
+mapKey("J", ":m '>+1<CR>gv=gv", { mode = "x" })
+mapKey("K", ":m '<-2<CR>gv=gv", { mode = "x" })
+
+-- swap back/forward keymaps
+mapKey("<C-o>", "<C-i>")
+mapKey("<C-i>", "<C-o>")
