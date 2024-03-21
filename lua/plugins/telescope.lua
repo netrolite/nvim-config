d5cf08d5f429477143f58a954058d4a1b6e746dc
netrolite <netrolite@proton.me>
2024-03-21 07:10:49 +0300

    put prompt on top in telescope & sort results in ascending order

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,33 +1,35 @@
-local function config()
-end
-
 return {
-  "nvim-telescope/telescope.nvim",
-  config = function()
-    local telescope = require("telescope")
-    telescope.setup({
-      defaults = {
-        file_ignore_patterns = {
-          "%.git/"
-        }
-      },
-      pickers = {
-        find_files = {
-          hidden = true, -- include dotfiles
-        },
-      }
-    })
-  end,
-  dependencies = { "nvim-lua/plenary.nvim" },
-  branch = "0.1.x",
-  event = { "BufReadPre", "BufNewFile" },
-  keys = {
-    { "<leader>ff", ":Telescope find_files<CR>" },
-    { "<leader>fa", ":Telescope<CR>" },
-    { "<leader>fk", ":Telescope keymaps<CR>" },
-    { "<leader>fh", ":Telescope help_tags<CR>" },
-    { "<leader>fw", ":Telescope live_grep<CR>" },
-    { "<leader>fb", ":Telescope buffers<CR>" },
-    { "<leader>fz", ":Telescope current_buffer_fuzzy_find<CR>" },
-  }
+	"nvim-telescope/telescope.nvim",
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			defaults = {
+				sorting_strategy = "ascending",
+				layout_config = {
+					prompt_position = "top",
+				},
+				file_ignore_patterns = {
+					"%.git/",
+				},
+				prompt_prefix = "",
+			},
+			pickers = {
+				find_files = {
+					hidden = true, -- include dotfiles
+				},
+			},
+		})
+	end,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	branch = "0.1.x",
+	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{ "<leader>ff", ":Telescope find_files<CR>" },
+		{ "<leader>fa", ":Telescope<CR>" },
+		{ "<leader>fk", ":Telescope keymaps<CR>" },
+		{ "<leader>fh", ":Telescope help_tags<CR>" },
+		{ "<leader>fw", ":Telescope live_grep<CR>" },
+		{ "<leader>fb", ":Telescope buffers<CR>" },
+		{ "<leader>fz", ":Telescope current_buffer_fuzzy_find<CR>" },
+	},
 }
