adee757c3a11b8027e611103066edf7b0f351c95
netrolite <netrolite@proton.me>
2024-03-22 19:35:05 +0300

    show diagnostics in bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,6 +7,10 @@ return {
 		bufferline.setup({
 			options = {
 				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level)
+					local icon = level:match("error") and " " or ""
+					return icon .. " " .. count
+				end,
 				offsets = {
 					{
 						filetype = "NvimTree",
