18e90ae9e72ee5535307d6387469c7f9e8076616
netrolite <netrolite@proton.me>
2024-03-29 11:37:12 +0300

    don't colorize literal color names like "red" or "blue"

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,7 +1,7 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	config = function()
-		require("colorizer").setup({ "*" })
+		require("colorizer").setup({ "*" }, { names = false })
 	end,
 	event = "VeryLazy",
 }
