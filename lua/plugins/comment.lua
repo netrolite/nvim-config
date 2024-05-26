4e85bc4d5524b9fb7d84c7ab915ea7920df58df5
netrolite <67281212+netrolite@users.noreply.github.com>
2024-05-26 09:34:52 +0300

    fix commenting out jsx not working

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,6 @@
 return {
 	"numToStr/Comment.nvim",
+	dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
 	keys = {
 		{
 			"<leader>/",
@@ -16,17 +17,34 @@ return {
 			mode = "v",
 		},
 	},
-	opts = {
-		toggler = {
-			line = "<leader>/",
-			block = "<nop>",
-		},
-		opleader = {
-			line = "<leader>/",
-			block = "<leader>b/",
-		},
-		mappings = {
-			extra = false, -- don't create unnecessary mappings
-		},
-	},
+	config = function()
+		require("Comment").setup({
+			toggler = {
+				line = "<leader>/",
+				block = "<nop>",
+			},
+			opleader = {
+				line = "<leader>/",
+				block = "<leader>b/",
+			},
+			mappings = {
+				extra = false, -- don't create unnecessary mappings
+			},
+			pre_hook = function(ctx)
+				local U = require("Comment.utils")
+
+				local location = nil
+				if ctx.ctype == U.ctype.block then
+					location = require("ts_context_commentstring.utils").get_cursor_location()
+				elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+					location = require("ts_context_commentstring.utils").get_visual_start_location()
+				end
+
+				return require("ts_context_commentstring.internal").calculate_commentstring({
+					key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+					location = location,
+				})
+			end,
+		})
+	end,
 }
