d69ddea2051cdd749c6be7ca8dd6ba870e99085e
netrolite <netrolite@proton.me>
2025-07-21 21:24:32 +0300

    treat .mdx files as .md files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,17 @@
 local create_autocmd = vim.api.nvim_create_autocmd
 
 create_autocmd("FileType", {
-	pattern = "*",
-	callback = function()
-		vim.cmd("set formatoptions-=cro")
-	end,
+  pattern = "*",
+  callback = function()
+    vim.cmd("set formatoptions-=cro")
+  end,
+})
+
+-- set markdown highlight for mdx file
+create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.mdx" },
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+  end,
 })
