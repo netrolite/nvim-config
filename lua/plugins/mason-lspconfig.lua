c8ac7542616ad850a596e33c76c317a56f27fb0b
netrolite <netrolite@proton.me>
2025-08-07 14:50:17 +0300

    fix rust analyzer not working outside of the initially opened buffer

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,11 @@
 return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
+    automatic_enable = false, -- this this this this shit fixes shit fucking finally
+    -- rust_analyzer was attaching twice without this and only worked for the buffer
+    -- that was open first
+
+
     automatic_installation = true,
     ensure_installed = {
       "gopls",
