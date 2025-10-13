16a8b835b16df4ef96e9b734f875a525970bf548
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-10-14 00:06:49 +0300

    refactor

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -13,6 +13,7 @@ return {
 
     require("nvim-dap-virtual-text").setup()
 
+
     vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
     vim.keymap.set("n", "<F1>", dap.continue)
     vim.keymap.set("n", "<Up>", dap.restart)
@@ -27,17 +28,9 @@ return {
 
 
     -- automatically open dap ui
-    dap.listeners.before.attach.dapui_config = function()
-      ui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      ui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      ui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      ui.close()
-    end
+    dap.listeners.before.attach.dapui_config = ui.open
+    dap.listeners.before.launch.dapui_config = ui.open
+    dap.listeners.before.event_terminated.dapui_config = ui.close
+    dap.listeners.before.event_exited.dapui_config = ui.close
   end
 }
